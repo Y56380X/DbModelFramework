1e892690dd5a1396da1ede808b589e89f4a77a72
Y56380X <Y56380X@users.noreply.github.com>
2020-12-21 23:31:27 +0100

    minor ModelProperty refactorings

--- src/DbModelFramework/ModelProperty.cs
+++ src/DbModelFramework/ModelProperty.cs
@@ -1,5 +1,5 @@
-﻿/**
-	Copyright (c) 2017 Y56380X
+﻿/*
+	Copyright (c) 2017-2020 Y56380X
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +18,7 @@
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
-**/
+*/
 
 using System;
 using System.Data;
@@ -26,39 +26,37 @@ using System.Reflection;
 
 namespace DbModelFramework
 {
-	public class ModelProperty
+	public sealed class ModelProperty
 	{
 		#region delegates
 
-		delegate object ConvertValue(object value, Type targetType);
+		private delegate object ConvertValue(object value, Type targetType);
 
 		#endregion
 
 		#region fields
 
-		private PropertyInfo property;
-		private MethodInfo foreignKeyLoader;
-		private ConvertValue convertValue;
+		private readonly PropertyInfo property;
+		private readonly MethodInfo foreignKeyLoader;
+		private readonly ConvertValue convertValue;
 
 		#endregion
 
 		#region properties
 
-		public virtual string PropertyName { get; private set; }
-		public virtual string AttributeName { get; private set; }
-		public virtual DbType Type { get; private set; }
-		public virtual object DefaultValue { get; private set; }
-		public virtual bool IsPrimaryKey { get; private set; }
-		public virtual bool IsUnique { get; private set; }
-		public virtual bool IsForeignKey { get; private set; }
-		public virtual ModelProperty ForeignKeyReference { get; private set; }
-		public virtual string ForeignKeyTableName { get; private set; }
+		public string PropertyName { get; }
+		public string AttributeName { get; }
+		public DbType Type { get; }
+		public object DefaultValue { get; }
+		public bool IsPrimaryKey { get; }
+		public bool IsUnique { get; }
+		public bool IsForeignKey { get; }
+		public ModelProperty ForeignKeyReference { get; }
+		public string ForeignKeyTableName { get; }
 
 		#endregion
 
 		#region constructors
-
-		internal ModelProperty() { }
 
 		public ModelProperty(PropertyInfo property)
 		{
@@ -72,7 +70,7 @@ namespace DbModelFramework
 			IsPrimaryKey = Attribute.IsDefined(property, typeof(PrimaryKeyAttribute));
 			IsUnique = Attribute.IsDefined(property, typeof(DbUniqueAttribute));
 
-			foreignKeyLoader = IsForeignKey ? baseClass.GetMethod("Get", new Type[] { ForeignKeyReference.property.PropertyType }) : null;
+			foreignKeyLoader = IsForeignKey ? baseClass.GetMethod("Get", new[] { ForeignKeyReference.property.PropertyType }) : null;
 
 			this.property = property;
 			convertValue = property.PropertyType.IsEnum ? (ConvertValue)ToEnumValue : ChangeType;
@@ -100,20 +98,17 @@ namespace DbModelFramework
 			}
 		}
 
-		public object GetValue(object model)
-		{
-			if (IsForeignKey)
-				return ForeignKeyReference.GetValue(property.GetValue(model));
-			else
-				return property.GetValue(model);
-		}
+		public object GetValue(object model) => 
+			IsForeignKey 
+				? ForeignKeyReference.GetValue(property.GetValue(model)) 
+				: property.GetValue(model);
 
-		private static ModelProperty GetForeignKeyReference(Type model)
+		private static ModelProperty GetForeignKeyReference(IReflect model)
 		{
 			return (ModelProperty)model.GetField("PrimaryKeyProperty", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
 		}
 
-		private static string GetForeignKeyTableName(Type model)
+		private static string GetForeignKeyTableName(IReflect model)
 		{
 			return (string)model.GetField("TableName", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
 		}
